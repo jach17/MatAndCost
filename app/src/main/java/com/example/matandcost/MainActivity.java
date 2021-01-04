acc1274fe3e006e2bf48cd73e23b30926147fb84
jach17 <jonathan.aldana170500@gmail.com>
2021-01-03 21:52:27 -0600

    PruebaBtn

--- app/src/main/java/com/example/matandcost/MainActivity.java
+++ app/src/main/java/com/example/matandcost/MainActivity.java
@@ -3,12 +3,45 @@ package com.example.matandcost;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
+    Button btnLogin;
+    EditText txtUser, txtPassword;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        btnLogin = (Button)findViewById(R.id.btnLogin);
+        txtUser = (EditText)findViewById(R.id.txtUser);
+        txtPassword= (EditText)findViewById(R.id.txtPassword);
+
+
+        btnLogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String userInput = txtUser.getText().toString();
+                String passwordInput = txtPassword.getText().toString();
+                if(auth(userInput, passwordInput)){
+                    Toast.makeText(MainActivity.this, "Hola, Admin!", Toast.LENGTH_SHORT).show();
+                }else{
+                    Toast.makeText(MainActivity.this, "Quién demonios eres??", Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
+    }
+    
+    public boolean auth(String user, String password){
+        String admin="admin";
+        String passAdmin="admin";
+        if(user.equals(admin) && passAdmin.equals(password)){
+            return true;
+        }else{
+            return false;
+        }
     }
 }
